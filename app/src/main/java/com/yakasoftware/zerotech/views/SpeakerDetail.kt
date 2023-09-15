98b1c4c984c1d8839df495547b285aa2f91ea8f5
Duman01x <cakiciaykut97@gmail.com>
2023-09-16 01:14:41 +0300

    Ürün detayları yolladım gardaş

--- app/src/main/java/com/yakasoftware/zerotech/views/SpeakerDetail.kt
+++ app/src/main/java/com/yakasoftware/zerotech/views/SpeakerDetail.kt
@@ -1,8 +1,25 @@
 package com.yakasoftware.zerotech.views
 
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -10,8 +27,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import androidx.navigation.NavHostController
 import com.google.firebase.firestore.ktx.firestore
@@ -44,10 +66,89 @@ fun SpeakerDetailScreen(navController: NavHostController, productTitle: String) 
                     discount.value = document.getString("discount")!!
                 }
 
-        }
-        Column {
-            Text(text = productTitle, color = MaterialTheme.colorScheme.secondary)
+            }
+        Column(modifier = Modifier.fillMaxSize()){
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(350.dp)
+                    .border(BorderStroke(5.dp, MaterialTheme.colorScheme.onSecondary)),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .border(BorderStroke(5.dp, MaterialTheme.colorScheme.onSecondary))
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Transparent,//
+                                MaterialTheme.colorScheme.onPrimary
+                            ),
+                            startY = 0f,
+                            endY = 800f
+                        )
+                    )){
 
+                    //Resimler bu kısıma eklenecek - ayarlama yapılacak sonrasında
+                   // Image(painter = , contentDescription = )
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            //Ürünün İsmi
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Center
+            ){
+            Text(text = productTitle, color = MaterialTheme.colorScheme.secondary, textAlign = TextAlign.Center, fontSize = 20.sp)
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            //Yorum ve Yıldız ve FİYAT bilgisi
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+
+                //Yıldızlar
+                repeat(5) {
+                    Box(
+                        modifier = Modifier
+                            .size(25.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.StarBorder,
+                            contentDescription = "Sepetim",
+                            tint = MaterialTheme.colorScheme.onSecondary
+                        )
+                    }
+                }
+
+                //Yorum sayısı - Puanı - FİYAT BİLGİSİ
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+
+                ){
+                    Text(text = "(0) Yorum - 0 puan", fontSize = 10.sp, color = MaterialTheme.colorScheme.secondary)
+
+                    //FİYAT BİLGİSİ
+                    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
+                        Text(text = "3131$", fontSize = 30.sp)
+                    }
+
+                }
+
+            }
         }
     }
 }
